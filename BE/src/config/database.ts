e856cb6c94019a9025822c11c7d6c40763cbf7bd
dungnt <dungnt@solashi.com>
2026-07-13 16:43:13 +0700

    chore(deployment): auto enable SSL when connecting to TiDB Cloud

--- BE/src/config/database.ts
+++ BE/src/config/database.ts
@@ -20,7 +20,9 @@ const config = {
   migrations: [__dirname + '/../database/migrations/**/*{.ts,.js}'],
   charset: 'utf8mb4',
   logging: false,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
+  ssl: (process.env.DB_SSL === 'true' || (process.env.DB_HOST && process.env.DB_HOST.includes('tidbcloud.com')))
+    ? { rejectUnauthorized: false }
+    : undefined,
 };
 
 export default registerAs('database', () => config);
